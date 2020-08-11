a2087be6181c51fea78a657a97dbfad68026ab00
Unknown <kfo58534@gmail.com>
2020-08-11 09:56:30 +0800

    alamofire install

---
+++ OrderDrink/DrinkListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DrinkListViewController.swift
+//  OrderDrink
+//
+//  Created by Una Lee on 2020/8/10.
+//  Copyright © 2020 una.lee. All rights reserved.
+//
+
+import UIKit
+
+class DrinkListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func viewInit() {
+        
+    }
+}
---
+++ Pods/Target Support Files/Pods-OrderDrink/Pods-OrderDrink-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OrderDrink : NSObject
+@end
+@implementation PodsDummy_Pods_OrderDrink
+@end
